54e4e22af395fea10abc6f2a8dd0b27699600dfe
Antonio <antony.dovzhenko@gmail.com>
2024-04-03 16:10:05 +0300

    Added numpy, endpoint and changed dockerfile back to optimized version

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
@@ -6,3 +7,17 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get("/multiply-matrices")
+async def multiply_matrices():
+    matrixA = numpy.random.rand(10, 10)
+    matrixB = numpy.random.rand(10, 10)
+
+    product = numpy.dot(matrixA, matrixB)
+
+    return {
+        "matrixA": matrixA.tolist(),
+        "matrixB": matrixB.tolist(),
+        "product": product.tolist(),
+    }
